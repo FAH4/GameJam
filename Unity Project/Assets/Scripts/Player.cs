08d31079f0e64b59142bfe758a2bbc3d05fa13cf
Hunkenstein <fhamilt1@hotmail.com>
2014-12-06 11:49:32 -0500

    Right Analog Stick, Player Rotation

--- Unity Project/Assets/Scripts/Player.cs
+++ Unity Project/Assets/Scripts/Player.cs
@@ -5,12 +5,23 @@ public class Player : MonoBehaviour {
 	public enum PlayerMovementCurveTypes {Linear_Time, Log_Time, Exp_Time};
 	public Camera MainCamera;
 	Vector2 PlayerPosition;
+	Vector3 PlayerRotation;
 	Vector2 PlayerSpeed;
 	Vector2 PlayerPreviousSpeed;
-	public float PlayerSpeedModifier = 1;
 
+	[Header("Rotation Setings")]
+	public float PlayerXRotationAmount = 5;
+	public float PlayerYRotationAmount = 30;
+	public bool RotateShipInZWithXMovement = true;
+	public bool RotateShipInYWithXMovement = true;
+	public bool RotateShipInXWithXMovement = true;
+	public bool RotateShipInZWithYMovement = true;
+	public bool RotateShipInYWithYMovement = true;
+	public bool RotateShipInXWithYMovement = true;
 	//public PlayerMovementCurveTypes PlayerMovementCurve = PlayerMovementCurveTypes.Log_Time;
-	
+
+	[Header("Movement Settings")]
+	public float PlayerSpeedModifier = 1;
 	[Range (0,1)]
 	public float PlayerLogarithmicSmoothingWithInput =.5f;
 	[Range (0,1)]
@@ -18,19 +29,23 @@ public class Player : MonoBehaviour {
 	[Range (0,1)]
 	public float ControllerDeadZone =.1f;
 	
-	
+	[Header("Weapon Settings")]
 	public bool FollowMouse = false;
+	public bool RightAnaloStick = true;
 	public float WeaponCooldown;
 	public float BulletSpeed;
 	private float WeaponTimer;
 	private GameObject TempBullet;
-	private Vector3 MousePosition;
+	private Vector3 BulletLookAtLocation;
+	private Vector3 OriginalRotation;
 	
 	//public float PlayerLinearTimeToReachSpeed;
 	
 	// Use this for initialization
 	void Start () {
 		EnemyManager.Instance.SpawnEnemy ();
+		PlayerRotation = this.transform.eulerAngles;
+		OriginalRotation = PlayerRotation;
 	}
 	
 	// Update is called once per frame
@@ -40,9 +55,11 @@ public class Player : MonoBehaviour {
 	}
 	void PlayerMovement(){
 		PlayerPosition = this.transform.localPosition;
+
 		MoveShip ();
 		CheckInPlayArea ();
 		this.transform.localPosition = PlayerPosition;
+		this.transform.eulerAngles = PlayerRotation;
 		PlayerPreviousSpeed = PlayerSpeed;
 	}
 	void PlayerFire(){
@@ -55,12 +72,21 @@ public class Player : MonoBehaviour {
 			TempBullet.transform.forward=(this.transform.right);
 
 			if(FollowMouse){
-				MousePosition = MainCamera.ScreenToWorldPoint(Input.mousePosition);
-				MousePosition.z =this.transform.position.z;
+				BulletLookAtLocation = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+				BulletLookAtLocation.z =this.transform.position.z;
 
-				TempBullet.transform.LookAt(MousePosition - this.transform.position);
+				TempBullet.transform.LookAt(BulletLookAtLocation);
 
-				Debug.Log(MousePosition);
+
+			}
+			if(RightAnaloStick){
+				BulletLookAtLocation.x = this.transform.position.x + Input.GetAxis ("HorizontalRightStick");
+				BulletLookAtLocation.y = this.transform.position.y + Input.GetAxis("VerticalRightStick");
+				BulletLookAtLocation.z = 0;
+				
+				TempBullet.transform.LookAt(BulletLookAtLocation);
+				
+
 			}
 		}
 	}
@@ -77,6 +103,15 @@ public class Player : MonoBehaviour {
 		}
 		PlayerPosition.x += PlayerSpeed.x * PlayerSpeedModifier * Time.deltaTime;
 		PlayerPosition.y += PlayerSpeed.y * PlayerSpeedModifier * Time.deltaTime;
+		RotateShip ();
+	}
+	void RotateShip(){
+		if (RotateShipInZWithYMovement)	PlayerRotation.z = PlayerSpeed.y * PlayerYRotationAmount + OriginalRotation.z;
+		if (RotateShipInYWithYMovement)	PlayerRotation.y = PlayerSpeed.y * PlayerYRotationAmount + OriginalRotation.y;
+		if (RotateShipInXWithYMovement)	PlayerRotation.x = PlayerSpeed.y * PlayerYRotationAmount + OriginalRotation.x;
+		if (RotateShipInZWithXMovement)	PlayerRotation.z = PlayerSpeed.x * PlayerXRotationAmount + OriginalRotation.z;
+		if (RotateShipInYWithXMovement)	PlayerRotation.y = PlayerSpeed.x * PlayerXRotationAmount + OriginalRotation.y;
+		if (RotateShipInXWithXMovement)	PlayerRotation.x = PlayerSpeed.x * PlayerXRotationAmount + OriginalRotation.x;
 	}
 	void CheckInPlayArea(){
 		PlayerPosition.x = PlayerPosition.x < -.5f ? -.5f : PlayerPosition.x;
